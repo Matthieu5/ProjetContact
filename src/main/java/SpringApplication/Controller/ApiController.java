25dd1437b5dbf50264a8e4eaec5173acf0a9708d
matthieu <matthieuboulagnon@gmail.com>
2022-03-11 21:26:54 +0100

    Ajout add & edit contact

--- src/main/java/SpringApplication/Controller/ApiController.java
+++ src/main/java/SpringApplication/Controller/ApiController.java
@@ -4,14 +4,14 @@ import SpringApplication.data.ContactRepository;
 import SpringApplication.data.MailRepository;
 import SpringApplication.model.Contact;
 import SpringApplication.model.Mail;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -45,6 +45,40 @@ public class ApiController {
                         contact.get().getMails().clear();
                         contactRrepository.deleteById(id);
                     }
+            }
+        }
+
+        return null;
+    }
+
+    @PostMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<Object> postInfo(@RequestParam String action, @RequestParam(defaultValue = "0") long id, @RequestBody String xml, Model model) {
+        if (action != null) {
+            Contact value = null;
+            switch (action) {
+                case "addContact":
+                    try {
+                        XmlMapper xmlMapper = new XmlMapper();
+                        value = xmlMapper.readValue(xml, Contact.class);
+                        contactRrepository.save(value);
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
+                    return new ResponseEntity<>(HttpStatus.OK);
+                case "editContact":
+                    try {
+                        XmlMapper xmlMapper = new XmlMapper();
+                        value = xmlMapper.readValue(xml, Contact.class);
+                        Contact oldContact = contactRrepository.getOne(id);
+                        oldContact.setPrenomC(value.getPrenomC());
+                        oldContact.setNomC(value.getNomC());
+                        oldContact.setAdresseC(value.getAdresseC());
+                        oldContact.setMails(value.getMails());
+                        contactRrepository.flush();
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
+                    return new ResponseEntity<>(HttpStatus.OK);
             }
         }
 
